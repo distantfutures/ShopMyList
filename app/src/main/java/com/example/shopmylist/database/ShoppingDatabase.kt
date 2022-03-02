dc8e06934eb533ea19a2a20b37ab66631262d10b
Andrew Zheng <andzheng@gmail.com>
2022-03-02 14:51:01 -0500

    Implement ShoppingDatabase

---
+++ app/src/main/java/com/example/shopmylist/database/ShoppingDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.shopmylist.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingItem::class], version = 1, exportSchema = false)
+abstract class ShoppingDatabase: RoomDatabase() {
+
+    abstract fun getShoppingDao(): ShoppingDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ShoppingDatabase? = null
+
+        fun getInstance(context: Context): ShoppingDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+                // If instance is `null` make a new database instance.
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ShoppingDatabase::class.java,
+                        "discharge_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    // Assign INSTANCE to the newly created database.
+                    INSTANCE = instance
+                }
+                // Return instance; smart cast to be non-null.
+                return instance
+            }
+        }
+    }
+}
