147a6f20b6ecf7bca2f9ed7f682cf7445d176dd9
Andrew Zheng <andzheng@gmail.com>
2022-03-02 17:44:51 -0500

    Added dialog classes

--- app/src/main/java/com/example/shopmylist/ui/AddDialogListener.kt
+++ app/src/main/java/com/example/shopmylist/ui/AddDialogListener.kt
@@ -1,4 +1,7 @@
 package com.example.shopmylist.ui
 
+import com.example.shopmylist.data.database.entities.ShoppingItem
+
 interface AddDialogListener {
+    fun onAddButtonClicked(item: ShoppingItem)
 }
--- app/src/main/java/com/example/shopmylist/ui/AddShoppingItemDialog.kt
+++ app/src/main/java/com/example/shopmylist/ui/AddShoppingItemDialog.kt
@@ -1,4 +1,33 @@
 package com.example.shopmylist.ui
 
-class AddShoppingItemDialog {
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDialog
+import com.example.shopmylist.data.database.entities.ShoppingItem
+import kotlinx.android.synthetic.main.dialog_add_shopping_item.*
+
+class AddShoppingItemDialog(context: Context, var addDialogListener: AddDialogListener) : AppCompatDialog(context) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        textViewAdd.setOnClickListener{
+            val name = editTextName.text.toString()
+            var amount = editTextAmount.text.toString()
+
+            if (name.isEmpty()) {
+                Toast.makeText(context, "Please enter the item name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (amount.isEmpty()) {
+                amount = "1"
+            }
+            val item = ShoppingItem(name, amount.toInt())
+            addDialogListener.onAddButtonClicked(item)
+        }
+        textViewCancel.setOnClickListener {
+            cancel()
+        }
+    }
 }
